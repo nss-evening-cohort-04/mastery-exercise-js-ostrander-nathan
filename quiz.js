9e668554f36a34542aee3ad08eb5fde797d0eac2
Nathan Ostrander <ostrander_nathan@yahoo.com>
2016-08-14 15:27:53 -0500

    first quiz push

---
+++ quiz.js
@@ -0,0 +1,33 @@
+// var declared
+var height = "";
+var width = "";
+var obj = {
+	key1: "height", 
+	key2: "width"
+};
+var tree = "*";
+var space = "";
+// set up link to html
+// document.getElementById("treeBtn").addEventListener("click", 
+// 	validateForm()){
+//     document.getElementById("treeBtn").innerHTML = "Tree Growing";
+// };
+// function for form validation
+// function validateForm() {
+//     var x = document.button["treeBtn"].value;
+//     if (x == null || x == "") {
+//         alert("Both fields must have a value");
+//         return false;
+//     }
+// }
+// validateForm();
+
+// function for tree grow
+function treeFun() {
+	for (var i = 0; i < tree.length; i++) {
+		space = space + tree[i];  {
+        }
+		}
+		console.log(space);
+}
+treeFun();
