af8302a06d07448222084c8b8dd4125c55de83c1
Nathan Ostrander <ostrander_nathan@yahoo.com>
2016-08-14 19:27:18 -0500

    second quiz attempt

--- quiz.js
+++ quiz.js
@@ -7,6 +7,24 @@ var obj = {
 };
 var tree = "*";
 var space = "";
+var height = document.getElementById("height");
+var width = document.getElementById("width");
+var numberOfi  = "";
+function height() {
+	var heightInput = document.getElementById('height');
+	outputDiv.innerHTML = height;
+}
+	// height.addEventListener("key1", function(event) {
+ //    event.preventDefault();
+ //    if (event.key1 == 13)
+ //        height.click();
+// });
+
+// var keys = function() {  }
+
+// element.attachEvent('onclick', myFunctionReference);
+// element.addEventListener('click', myFunctionReference , false);
+
 // set up link to html
 // document.getElementById("treeBtn").addEventListener("click", 
 // 	validateForm()){
@@ -31,3 +49,15 @@ function treeFun() {
 		console.log(space);
 }
 treeFun();
+// new function no stack getting weird return 
+function newTree(width, height) {
+    var start = 2, html = '';
+    for (var i=width; i--;) {
+        html += new Array(Math.floor(i+1)).join(' ') + new Array((start=start+2)-2).join(height);
+    }
+    return html + '';
+    var width = height.replace(/(.{6})/g, '').trim();
+	console.log(width + height);
+}
+console.log( newTree(9,'\n', '*') );
+newTree();
