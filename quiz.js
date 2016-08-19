919c4f1749b84477784dfd45610bdf1e7dcff5d3
Nathan Ostrander <ostrander_nathan@yahoo.com>
2016-08-19 17:18:26 -0500

    getting close still issues with char input

--- quiz.js
+++ quiz.js
@@ -1,63 +1,33 @@
-// var declared
-var height = "";
-var width = "";
-var obj = {
-	key1: "height", 
-	key2: "width"
-};
-var tree = "*";
-var space = "";
-var height = document.getElementById("height");
-var width = document.getElementById("width");
-var numberOfi  = "";
-function height() {
-	var heightInput = document.getElementById('height');
-	outputDiv.innerHTML = height;
-}
-	// height.addEventListener("key1", function(event) {
- //    event.preventDefault();
- //    if (event.key1 == 13)
- //        height.click();
-// });
 
-// var keys = function() {  }
+var grow = document.getElementById("treeGrow");
 
-// element.attachEvent('onclick', myFunctionReference);
-// element.addEventListener('click', myFunctionReference , false);
+var myObject = {height: "number",character: "inputChar"};
+console.log(myObject);
 
-// set up link to html
-// document.getElementById("treeBtn").addEventListener("click", 
-// 	validateForm()){
-//     document.getElementById("treeBtn").innerHTML = "Tree Growing";
-// };
-// function for form validation
-// function validateForm() {
-//     var x = document.button["treeBtn"].value;
-//     if (x == null || x == "") {
-//         alert("Both fields must have a value");
-//         return false;
-//     }
-// }
-// validateForm();
-
-// function for tree grow
-function treeFun() {
-	for (var i = 0; i < tree.length; i++) {
-		space = space + tree[i];  {
-        }
+function anotherTree(clickevent){
+	console.log("event", clickevent);
+	var number = document.getElementById('number').value
+	var inputChar = document.getElementById("inputChar").value
+	var consoleResults="";
+	
+	for (var i = 0; i < number; i++) {
+		var stars = "";
+		for (var j = 0; j < number - i; j++) {
+			stars = stars + ' ';
 		}
-		console.log(space);
+		for (var k = 0; k < 2 * i - 1; k++) {
+			stars = stars + "*"
+		}
+		consoleResults += stars;
+	console.log(stars);
+	}
 }
-treeFun();
-// new function no stack getting weird return 
-function newTree(width, height) {
-    var start = 2, html = '';
-    for (var i=width; i--;) {
-        html += new Array(Math.floor(i+1)).join(' ') + new Array((start=start+2)-2).join(height);
-    }
-    return html + '';
-    var width = height.replace(/(.{6})/g, '').trim();
-	console.log(width + height);
-}
-console.log( newTree(9,'\n', '*') );
-newTree();
+
+grow.addEventListener("click", anotherTree);
+
+// var person = {};
+// var key = "name";
+
+// person[key]  this is same as person.name  = "John";
+
+// console.log(person);
